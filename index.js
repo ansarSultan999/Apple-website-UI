a1bd226dc0171c4e4b37b7b11af1007f370a4be5
ansarSultan999 <ansarsultan2002@gmail.com>
2023-10-21 18:46:25 +0500

    working on this UI

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Initialize Swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    loop:true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+  });
